f0b844299d24e66fe469cea66ffb038613d68204
sseang <142060734+sseang@users.noreply.github.com>
2023-12-29 19:22:48 -0600

    A GET request results in the most recent result being renedered

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,49 +5,60 @@ console.log('client.js is sourced!');
 
 // #### Client-Side:
 
-// * []Inside the `<section data-testid="resultHistory">` element, display a list of all previous calculations on the page when it loads (using a `GET '/calculations'` request). 
-    //fetch data function or GET request
-    function fetchMath() {
-        //confirm function
-        console.log('In the FETCH function');
-        //axios GET
-        axios ({
-            method: 'GET',
-            url: '/calculations'
-        })
-        .then((response) => {
-            console.log(response);
-            //   * []Update this list when a new calculation is made.
-            //target data
-            let mathFromServer = response.data;
-            //confirm data
-            console.log(mathFromServer);
-        //target element and store in variable
-            let historyDiv = document.querySelector('#oldHistory');
-            //confirm data
-            console.log(historyDiv);
-            //loop and rendor to DOM
-            for (let items of mathFromServer) {
-                //confirm data
-                console.log(items);
-                historyDiv.innerHTML += `
-                <li>
-                    <i>${items.numOne}${items.operator}${items.numTwo} = ${items.result}</i>
-                </li>
-                `;     
-            }
-        })
-        .catch((error) => {
-            console.log(error);
-            alert('NO!! Check console for details.');
-        });
-    };
-    
-    fetchMath();
-    
-
-// * []Inside the `<section data-testid="recentResult">` element, display the most recent calculation **result**.
+// * [x]Inside the `<section data-testid="recentResult">` element, display the most recent calculation **result**.
 //   * []Update this when a new calculation is made.
+//fetch data function or GET request
+function fetchMath() {
+    //confirm function
+    console.log('In the FETCH function');
+    //axios GET
+    axios ({
+        method: 'GET',
+        url: '/calculations'
+    })
+    .then((response) => {
+        console.log(response);
+        //target data
+        let mathFromServer = response.data;
+        //confirm data
+        console.log(mathFromServer);
+        //target element and store in variable
+        //let historyDiv = document.querySelector('#oldHistory');
+        let recentDiv = document.querySelector('#recentHistory')
+        //confirm data
+        //console.log(historyDiv);
+        console.log(recentDiv);
+        
+        //loop and rendor to DOM
+        for (let items of mathFromServer) {
+            //confirm data
+            console.log(items);
+
+            //not able to do 2 GET calls?
+            // historyDiv.innerHTML += `
+            // <li>
+            //     <i>${items.numOne}${items.operator}${items.numTwo} = ${items.result}</i>
+            // </li>
+            // `; 
+            recentDiv.innerHTML += `
+            <li>
+            <i>${items.numOne}${items.operator}${items.numTwo} = ${items.result}</i>
+            </li>
+            `;       
+        }
+    })
+    .catch((error) => {
+        console.log(error);
+        alert('NO!! Check console for details.');
+    });
+};
+
+fetchMath();
+
+// * []Inside the `<section data-testid="resultHistory">` element, display a list of all previous calculations on the page when it loads (using a `GET '/calculations'` request). 
+//   * []Update this list when a new calculation is made.
+
+
 
 // * []Inside `<form data-testid="calculator">`:
 //   * []Create a user interface where the user can input two values and select a mathematical operator.
